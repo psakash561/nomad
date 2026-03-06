38cbcc3e4c20242e4c763575bfc2bd8b010e1717
Akash <akash@Akashs-MacBook-Air.local>
2026-03-06 16:08:11 +0530

    fix terraform working directory

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,19 @@
-data "aws_availability_zones" "available" {}
+data "aws_availability_zones" "available" {
+  state = "available"
+}
 
 # --- 1. NETWORK (VPC) ---
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.0.0"
-  name    = "nomad-vpc"
-  cidr    = "10.0.0.0/16"
-  azs = slice(data.aws_availability_zones.available.names, 0, 2)
 
+  name = "nomad-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = slice(data.aws_availability_zones.available.names, 0, 2)
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+}
 
   enable_nat_gateway   = true
   single_nat_gateway   = true
