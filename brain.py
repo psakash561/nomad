2d75465b12a471d096f2e6e5b4e4710670ed940a
Akash <akash@Akashs-MacBook-Air.local>
2026-03-07 13:44:49 +0530

    Fix brain.py structure

--- brain.py
+++ brain.py
@@ -1,3 +1,23 @@
+import subprocess
+import scout
+
+
+def get_current_region():
+    with open("terraform/terraform.tfvars") as f:
+        for line in f:
+            if "target_region" in line:
+                return line.split('"')[1]
+
+
+def set_region(region):
+    with open("terraform/terraform.tfvars", "w") as f:
+        f.write(f'target_region = "{region}"')
+
+    subprocess.run(["git", "add", "."])
+    subprocess.run(["git", "commit", "-m", f"Nomad migration to {region}"])
+    subprocess.run(["git", "push"])
+
+
 def main():
 
     cheapest_region = scout.find_cheapest()
@@ -25,10 +45,6 @@ def main():
 
     print("Region updated. CI/CD will migrate infrastructure.")
 
-    def set_region(region):
-    with open("terraform/terraform.tfvars", "w") as f:
-        f.write(f'target_region = "{region}"')
 
-subprocess.run(["git", "add", "."])
-subprocess.run(["git", "commit", "-m", f"Nomad migration to {region}"])
-subprocess.run(["git", "push"])
+if __name__ == "__main__":
+    main()
