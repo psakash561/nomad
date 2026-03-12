46b2c4517ce17ce52c3c2423647d5641eb0e9d79
Akash <akash@Akashs-MacBook-Air.local>
2026-03-12 17:35:38 +0530

    trigger migration pipeline

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,7 +2,3 @@ variable "target_region" {
   description = "Region where Nomad deploys infrastructure"
   type        = string
 }
-variable "aws_region" {
-  description = "AWS region where infrastructure is deployed"
-  type        = string
-}
